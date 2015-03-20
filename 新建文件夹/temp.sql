fb0718d8e29fc5e51172d27b798d5c14e7c3e556
RiverXiong <riverxiong@live.com>
2015-03-20 15:01:43 +0800

    删除空文件

--- 新建文件夹/temp.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table menutree...
-set feedback off
-set define off
-insert into menutree (MENU_ID, MENUPARENT_ID, MENU_ORDER)
-values ('F4647D26-5183-6F46-87FF-FBEEF4BCFA21', 'F274B7DB-48C1-014F-8D12-A9E573AA7016', 13);
-
-prompt Done.
